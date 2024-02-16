1d529849647a2730d539253c7741f0d78bc417d5
kcss-ashvini <134584325+kcss-ashvini@users.noreply.github.com>
2024-02-16 16:11:56 +0530

    Interface updated

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,6 @@
 export interface ZohoDeskPortalSDKPlugin {
   initialise(options: { orgId: string, appId: string, dc: string }): Promise<{ value: string }>;
+  setUserToken(options: { userToken: string }): Promise<void>;
+  setJWTToken(options: { jwtToken: string }): Promise<void>;
+  isUserSignedIn(): Promise<{ signedIn: boolean }>;
 }
